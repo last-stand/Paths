a7eeb2c60c5957024a51a595e60d5be41ce81d8a
Jai Prakash <jaiprak@thoughtworks.com>
2015-03-03 11:26:08 +0530

    Paths version 1 is done./Main class should not created.

--- Paths.java
+++ Paths.java
@@ -1,4 +1,24 @@
 import java.util.*;
 
 public class Paths{
+	static Map<String,List<String>> routes = new Hashtable<String,List<String>>();
+	static{
+		List<String> bangalore = new ArrayList<String>();
+		bangalore.add("singapore");
+
+		routes.put("bangalore",bangalore);
+	}
+
+	public static boolean isCityPresent(String city){
+		Set<String> keyset = routes.keySet();
+		if(keyset.contains(city)) return true;
+		else{
+			for (String src: keyset) {
+				List<String> destinations = routes.get(src);
+				if(destinations.contains(city))
+					return true;	
+			}
+		}
+		return false;
+	}
 }
--- PathsTest.java
+++ PathsTest.java
@@ -3,5 +3,27 @@ import static org.junit.Assert.*;
 import java.util.*;
 
 public class PathsTest {
-    
+    @Test
+    public void test_isCityPresent_should_return_true_if_bangalore_present_in_map(){
+        Paths path = new Paths();
+        assertTrue(path.isCityPresent("bangalore"));
+    }
+
+    @Test
+    public void test_isCityPresent_should_return_true_if_singapore_present_in_map(){
+        Paths path = new Paths();
+        assertTrue(path.isCityPresent("singapore"));
+    }
+
+    @Test
+    public void test_isCityPresent_should_return_false_if_london_do_not_present_in_map(){
+        Paths path = new Paths();
+        assertFalse(path.isCityPresent("london"));
+    }
+
+    @Test
+    public void test_isCityPresent_should_return_false_if_pune_do_not_present_in_map(){
+        Paths path = new Paths();
+        assertFalse(path.isCityPresent("pune"));
+    }
 }
